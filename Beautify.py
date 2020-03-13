051b16b646ec94b7884fc62a47beb4787e44f88f
Arne <six.arne@gmail.com>
2020-03-13 18:37:50 +0100

    finished basic scraper

--- Beautify.py
+++ Beautify.py
@@ -2,28 +2,69 @@ from bs4 import BeautifulSoup
 
 
 class Beautify:
+    __raw_content = None
     __content = None
-    __tag = None
-    __class = None
-    __id = None
 
-    def __init__(self, **config):
-        self.__tag = config['tag_name'] or None
-        self.__class = config['class_name'] or None
-        self.__id = config['id_name'] or None
+    __death_count = 0
+    __infected_count = 0
+    __recovered_count = 0
 
-    def load_content(self, content):
-        self.__content = content
+    __statistics_template = [
+        "country",
+        "total_cases",
+        "new_cases",
+        "total_deaths",
+        "new_deaths",
+        "total_recovered",
+        "active_cases",
+        "serious_critical",
+        "total_cases_per_1_million"
+    ]
 
-    def find_data(self):
-        try:
-            if not self.__content:
-                raise TypeError
+    __statistics_dict = {}
 
-            html = BeautifulSoup(self.__content, 'html.parser')
+    def __init__(self, content):
+        self.__raw_content = content
 
-            for index, tag in enumerate(html.select(f"{self.__tag}.{self.__class}")):
-                print(html.find_all("div", class_="primary_info"))
+    def parse_content(self):
+        self.__content = BeautifulSoup(self.__raw_content, 'html.parser')
 
-        except TypeError as err:
-            print(f"None value encountered")
+    def filter_main_counters(self):
+        counters = self.__content.find_all(id="maincounter-wrap")
+
+        for index, tag in enumerate(counters):
+            if index == 0:
+                self.__infected_count = tag.find('span').text
+            elif index == 1:
+                self.__death_count = tag.find('span').text
+            else:
+                self.__recovered_count = tag.find('span').text
+
+    def log_main_counters(self):
+        print(f"Infected: {self.__infected_count}")
+        print(f"dead: {self.__death_count}")
+        print(f"recovered: {self.__recovered_count}")
+
+    def parse_statistics(self, country):
+        table_rows = self.__content.find_all('tr')
+
+        for index, row in enumerate(table_rows):
+            if row.findChild().text.strip().lower() != country:
+                continue
+
+            cols = row.findChildren()
+
+            for i, col in enumerate(cols):
+                self.__statistics_dict[self.__statistics_template[i]] = col.text
+
+            break
+
+    def get_local_statistics(self):
+        return self.__statistics_dict
+
+    def get_global_statistics(self):
+        return {
+            "total_infected": self.__infected_count,
+            "total_dead": self.__death_count,
+            "total_recovered": self.__recovered_count,
+        }
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from Requester import Requester
 from Beautify import Beautify
+from Firestore import FirePost
+from time import sleep
 
 
 class PageScraper:
@@ -8,18 +10,44 @@ class PageScraper:
     __beautify = None
 
     def __init__(self, url):
-        try:
-            self.__requester = Requester(url)
-            self.__beautify = Beautify(tag_name="div", class_name="jam", id_name="test")
-
-        except KeyError as err:
-            print(f"Wrong key delivered {err}")
+        self.__requester = Requester(url)
 
     def initialize(self):
         self.__page_content = self.__requester.get_page_content()
-        self.__beautify.load_content(self.__page_content)
-        self.__beautify.find_data()
+        self.__beautify = Beautify(self.__page_content)
+
+    def run(self):
+        while True:
+            self.__beautify.parse_content()
+            self.__beautify.filter_main_counters()
+            self.__beautify.log_main_counters()
+            self.__beautify.parse_statistics('belgium')
+
+            local_data = self.__beautify.get_local_statistics()
+            global_data = self.__beautify.get_global_statistics()
+
+            FirePost.send_global_data_to_firebase(
+                total_dead=global_data['total_dead'],
+                total_infected=global_data['total_infected'],
+                total_recovered=global_data['total_recovered']
+            )
+
+            FirePost.send_local_data_to_firebase(
+                country=local_data['country'],
+                total_cases=local_data['total_cases'],
+                new_cases=local_data['new_cases'],
+                total_deaths=local_data['total_deaths'],
+                new_deaths=local_data['new_deaths'],
+                total_recovered=local_data['total_recovered'],
+                active_cases=local_data['active_cases'],
+                critical=local_data['serious_critical'],
+                total_case_per_million=local_data['total_cases_per_1_million']
+            )
+
+            # sleep 24 hours
+            sleep(60 * 60 * 24)
 
 
-scraper = PageScraper('localhost:3000')
+scraper = PageScraper('https://www.worldometers.info/coronavirus/')
 scraper.initialize()
+scraper.run()
